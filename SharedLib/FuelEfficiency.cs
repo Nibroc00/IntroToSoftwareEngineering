25236d35e69cb4b05a4249710f4275e9c9bce8ca
Corbin Park <corbin.park@ad.snow.edu>
2021-02-22 16:37:03 -0700

    Add project files.

---
+++ SharedLib/FuelEfficiency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SharedLib
+{
+    public class FuelEfficiency
+    {
+        public int calc_mpg(int miles, int gallons)
+        {
+            return 9;
+        }
+
+    }
+}
